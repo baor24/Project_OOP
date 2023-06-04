811a41adb796de47819ff00a1811df511115750b
HuyCao3110 <103634383+HuyCao3110@users.noreply.github.com>
2023-06-04 15:27:25 +0700

    Update GameFrame.java
    Delete menu in GameFrame, change game frame icon

--- src/GameDisplay/GameFrame.java
+++ src/GameDisplay/GameFrame.java
@@ -1,13 +1,7 @@
 package GameDisplay;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 
 import Model.GameMenuGUI;
 import Model.LoadData;
@@ -19,80 +13,16 @@ public class GameFrame extends JFrame {
 
 	private GamePanel gamePanel;
 
-	private JMenuBar mnb;
-	private JMenu menu;
-	private JMenuItem basic, nomal, hard, newGame, exit;
-
 	public GameFrame(int w, int h, int boom) {
 
 		loadData = new LoadData();
 
-		setJMenuBar(mnb = new JMenuBar());
-		mnb.add(menu = new JMenu("Game"));
-
-		menu.add(newGame = new JMenuItem("New game"));
-		menu.addSeparator();
-		menu.add(basic = new JMenuItem("Basic"));
-		menu.add(nomal = new JMenuItem("Nomal"));
-		menu.add(hard = new JMenuItem("Hard"));
-		menu.addSeparator();
-		menu.add(exit = new JMenuItem("Exit"));
-
-		if (w == 8) {
-			basic.setIcon(new ImageIcon(loadData.getListImage().get("tich")));
-		} else if (w == 16) {
-			nomal.setIcon(new ImageIcon(loadData.getListImage().get("tich")));
-		} else {
-			hard.setIcon(new ImageIcon(loadData.getListImage().get("tich")));
-		}
-
-		basic.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				setVisible(false);
-				new GameFrame(8, 8, 10);
-			}
-		});
-
-		nomal.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				setVisible(false);
-				new GameFrame(16, 16, 40);
-			}
-		});
-
-		hard.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				setVisible(false);
-				new GameFrame(16, 30, 99);
-			}
-		});
-
-		newGame.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				setVisible(false);
-				new GameFrame(w, h, boom);
-			}
-		});
-
-		exit.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
-			}
-		});
-
 		add(gamePanel = new GamePanel(w, h, boom, this));
 
-		setIconImage(loadData.getListImage().get("title"));
+		ImageIcon title = new ImageIcon("bomb.png");
+		
+		setIconImage(title.getImage());
+		setTitle("Minesweeper");
 		pack();
 		setResizable(false);
 		setLocationRelativeTo(null);
